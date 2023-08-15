77bc79e6d923c6e36561caa1f3f7e12e6b988faf
jatin sharma <93386551+Jatin4224@users.noreply.github.com>
2023-08-15 15:27:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ io.on("connection", (socket) => {
     console.log("A user disconnected");
   });
 });
-
-server.listen(3000, () => {
-  console.log("Listening on *:3000");
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on *:${port}`);
 });
+// server.listen(3000, () => {
+//   console.log("Listening on *:3000");
+// });
